df34a64ddd970872af5d7f05faf2bc457de16c8e
riskavmh <riska.vmh@gmail.com>
2021-06-13 22:20:25 +0700

    Praktikum ke-12 Web Framework

---
+++ Mingguu12 - CRUD 2/ci/application/views/Admin/_Template/content.php
@@ -0,0 +1,9 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading 
+    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>-->
+    <?php echo $isi; ?> <!-- untuk memanggil dan menampilkan isi dari konten yang sudah dibuat -->
+
+</div>
+<!-- /.container-fluid -->
