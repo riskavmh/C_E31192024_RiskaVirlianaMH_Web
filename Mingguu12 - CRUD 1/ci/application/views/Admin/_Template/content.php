1f811566facb31deb731c53ee4aba296e92a84db
riskavmh <66785957+riskavmh@users.noreply.github.com>
2021-06-13 22:13:52 +0700

    Delete Mingguu12 - CRUD 1/ci directory

--- Mingguu12 - CRUD 1/ci/application/views/Admin/_Template/content.php
+++
@@ -1,9 +0,0 @@
-<!-- Begin Page Content -->
-<div class="container-fluid">
-
-    <!-- Page Heading 
-    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>-->
-    <?php echo $isi; ?> <!-- untuk memanggil dan menampilkan isi dari konten yang sudah dibuat -->
-
-</div>
-<!-- /.container-fluid -->
